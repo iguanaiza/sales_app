2ae1786a6fa9209363d6e9ac6e777de1e246edb1
Iza <mrs.master.of.disaster@gmail.com>
2023-06-14 11:19:46 +0200

    Small update
    added grid in footer
    positioned buttons in footer
    closed form
    fixed hideForm event listener and function
    added minlenght in phone numbers

--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
 //HIDE/SHOW FORM/VERIFICATION
 function hideForm() {
-
-    const formValid = document.querySelector("#sales-form").checkValidity(); 
+    const appFormDiv = document.querySelector(".app-form");
+    const clear = document.querySelector(".switchVer");
+    const formValid = appForm.checkValidity(); 
 
     if (formValid == false) {
         return;
     }
-
-    const appForm = document.querySelector(".app-form");
-    const clear = document.querySelector(".switchVer");
-
+ 
     clear.classList.remove("switchVer");
-    appForm.classList.add("switchVer");
+    appFormDiv.classList.add("switchVer");
+
+    getData(); 
 }
 
 function hideVer() {
-    const appVer = document.querySelector(".app-ver");
+    const appVerDiv = document.querySelector(".app-ver");
     const clear = document.querySelector(".switchVer");
 
     clear.classList.remove("switchVer");
-    appVer.classList.add("switchVer");
+    appVerDiv.classList.add("switchVer");
 }
 
-//TAKE DATA FROM FORM
+//FORM FIELDS
 const formData = [
     "product",
     "title",
@@ -38,6 +38,7 @@ const formData = [
     "email"
 ]
 
+//TAKE DATA FROM FORM TO PUT INTO VERIFICATION
 function getData(){
 
     for (i=0; i<formData.length; i++) {
@@ -69,6 +70,13 @@ function getData(){
     }
 }
 
+//FORM
+const appForm = document.querySelector("#sales-form");
+
+appForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
 //BUTTONS
 const btnBack = document.querySelector("#btn-back");
 const btnCallback = document.querySelector("#btn-callback");
@@ -76,65 +84,5 @@ const btnNext = document.querySelector("#btn-next");
 const btnNo = document.querySelector("#btn-no");
 const btnOther = document.querySelector("#btn-other");
 
-btnNext.addEventListener("click", () => {    
-    hideForm();
-    getData();    
-});
-
-btnBack.addEventListener("click", hideVer);
-
-/*function getData(){
-    const formProduct = document.querySelector('input[name="product"]:checked').value;
-    const formTitle = document.querySelector('input[name="title"]:checked').value;
-    const formName = document.querySelector("#name").value;
-    const formSurname = document.querySelector("#surname").value;
-    const formZip = document.querySelector("#zip").value;
-    const formCity = document.querySelector("#city").value;
-    const formStreet = document.querySelector("#street").value;
-    const formHouse = document.querySelector("#house").value;
-    const formFlat = document.querySelector("#flat").value;
-    const formMobile = document.querySelector("#mobile").value;
-    const formPhone = document.querySelector("#phone").value;
-    const formEmail = document.querySelector("#email").value;
-
-    const verProduct = document.querySelector("#app-ver-product");
-    const verTitle = document.querySelector("#app-ver-title");
-    const verName = document.querySelector("#app-ver-name");
-    const verSurname = document.querySelector("#app-ver-surname");
-    const verZip = document.querySelector("#app-ver-zip");
-    const verCity = document.querySelector("#app-ver-city");
-    const verStreet = document.querySelector("#app-ver-street");
-    const verHouse = document.querySelector("#app-ver-house");
-    const verFlat = document.querySelector("#app-ver-flat");
-    const verMobile = document.querySelector("#app-ver-mobile");
-    const verPhone = document.querySelector("#app-ver-phone");
-    const verEmail = document.querySelector("#app-ver-email");
-
-    verProduct.innerHTML = formProduct;
-    verTitle.innerHTML = formTitle;
-    verName.innerHTML = formName;
-    verSurname.innerHTML = formSurname;
-    verZip.innerHTML = formZip;
-    verCity.innerHTML = formCity;
-    verStreet.innerHTML = formStreet;
-    verHouse.innerHTML = formHouse;
-    verMobile.innerHTML = formMobile;
-    
-    if (formFlat != 0) {
-        verFlat.classList.remove("hidden");
-        verFlat.innerHTML = '/ ' + formFlat;
-    }
-    else verFlat.classList.add("hidden");
-
-    if (formPhone != 0) {
-        verPhone.classList.remove("hidden");
-        verPhone.innerHTML = formPhone;
-    }
-    else verPhone.classList.add("hidden");
-
-    if (formEmail != 0) {
-        verEmail.classList.remove("hidden");
-        verEmail.innerHTML = formEmail;
-    }
-    else verEmail.classList.add("hidden");
-}*/
+btnNext.addEventListener("click", hideForm); //hides form, shows verification
+btnBack.addEventListener("click", hideVer); //hides verification, shows form
--- src/products.js
+++ src/products.js
@@ -1,4 +1,4 @@
-//PRODUCT LIST (max 5)
+//PRODUCT LIST (max 5 items, min. characters in name is 3)
 const products = [
     "Product 1",
     "Product 2",
@@ -8,7 +8,7 @@ const products = [
     "Product 6"
 ];
 
-//PRODUCT ASSIGNATION
+//PRODUCT ASSIGNATION FOR FORM
 for (i=0; i<products.length; i++) {
     if (products.length > 6) {
         break;
@@ -20,18 +20,10 @@ for (i=0; i<products.length; i++) {
     const formProduct = document.querySelector("#product"+x);
     const hideProduct = document.querySelector("#product-div-"+x);
 
-    if (products[i].length <= 3) {
+    if (products[i].length <= 3) { //min. product name lenght is 3 char.
         continue;
     }
 
     hideProduct.classList.remove("hidden");
     formProduct.innerHTML = products[i];
 }
-
-/*const formProduct1 = document.querySelector("#product1");
-const formProduct2 = document.querySelector("#product2");
-const formProduct3 = document.querySelector("#product3");
-
-formProduct1.innerHTML = products[0];
-formProduct2.innerHTML = products[1];
-formProduct3.innerHTML = products[2];*/
